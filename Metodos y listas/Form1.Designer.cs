51ec701396660bc9a9fb73eb48c1704a2b90ef5c
seba138 <s3bastian41@gmail.com>
2025-04-28 10:04:30 -0500

    Agregar archivos de proyecto.

---
+++ Metodos y listas/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace Metodos_y_listas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtElemento = new System.Windows.Forms.TextBox();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.lstElementos = new System.Windows.Forms.ListBox();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtElemento
+            // 
+            this.txtElemento.Location = new System.Drawing.Point(503, 79);
+            this.txtElemento.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtElemento.Name = "txtElemento";
+            this.txtElemento.Size = new System.Drawing.Size(132, 22);
+            this.txtElemento.TabIndex = 0;
+            this.txtElemento.TextChanged += new System.EventHandler(this.txtElemento_TextChanged);
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(715, 170);
+            this.btnAgregar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(100, 28);
+            this.btnAgregar.TabIndex = 1;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // lstElementos
+            // 
+            this.lstElementos.FormattingEnabled = true;
+            this.lstElementos.ItemHeight = 16;
+            this.lstElementos.Location = new System.Drawing.Point(503, 153);
+            this.lstElementos.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.lstElementos.Name = "lstElementos";
+            this.lstElementos.Size = new System.Drawing.Size(159, 116);
+            this.lstElementos.TabIndex = 2;
+            this.lstElementos.SelectedIndexChanged += new System.EventHandler(this.lstElementos_SelectedIndexChanged);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(715, 224);
+            this.btnEliminar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(100, 28);
+            this.btnEliminar.TabIndex = 3;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.lstElementos);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.txtElemento);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtElemento;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.ListBox lstElementos;
+        private System.Windows.Forms.Button btnEliminar;
+    }
+}
+
---
+++ Metodos y listas/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Metodos_y_listas
+{
+    public partial class Form1 : Form
+    {
+        private List<string> elementos = new List<string>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txtElemento_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            string nuevoElemento = txtElemento.Text;
+            if (!string.IsNullOrWhiteSpace(nuevoElemento))
+            {
+                elementos.Add(nuevoElemento);
+                ActualizarLista();
+                txtElemento.Clear();
+            }
+            else
+            {
+                MessageBox.Show("Por favor ingresa un elemento válido.");
+            }
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            if (lstElementos.SelectedItem != null)
+            {
+                elementos.Remove(lstElementos.SelectedItem.ToString());
+                ActualizarLista();
+            }
+            else
+            {
+                MessageBox.Show("Selecciona un elemento para eliminar.");
+            }
+        }
+        private void ActualizarLista()
+        {
+            lstElementos.DataSource = null;
+            lstElementos.DataSource = elementos;
+        }
+
+        private void lstElementos_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
